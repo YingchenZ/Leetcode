1c08da34c9f2c020597db9eab3ae9de1c47917a6
Yingchen (Eric) Zhou <ericzyc0803@gmail.com>
2021-04-06 09:53:17 +0800

    Update removeDuplicate.cpp

--- removeDuplicate.cpp
+++ removeDuplicate.cpp
@@ -13,19 +13,33 @@ int removeDuplicates(std::vector<int>& nums) {
     return nums.size();
 }
 
-// Before = {0,1,2,3,4,5,6,7,8,9}
-// After = {0,1,2,3,4}
+// Before = {1,1,1,2,2,3}
+// After  = {1,2,3}
+
+// Before = {1,1}
+// After  = {1}
+
+// Before = {1,2,3}
+// After  = {1,2,3}
+
+// Before = {0,0,1,1,1,1,2,3,3}
+// After  = {0,1,2,3}
 
 int main(){
-    std::vector<int>test = {0,0,1,1,1,2,2,3,3,4};
-    std::cout << "Before = {";
-    for(int i = 0; i < test.size(); i++){
-        std::cout << i << (i == (test.size() - 1)? "}\n":",");
-    }
-    removeDuplicates(test);
-    std::cout << "After = {";
-    for(int i = 0; i < test.size(); i++){
-        std::cout << i << (i == (test.size() - 1)? "}\n":",");
+    std::vector<std::vector<int>> testSet = {
+        {1,1,1,2,2,3}, {1,1},{1,2,3},{0,0,1,1,1,1,2,3,3}
+    };
+
+    for(std::vector<int> test : testSet){
+        std::cout << "Before = {";
+        for(int i = 0; i < test.size(); i++){
+            std::cout << test[i] << (i == (test.size() - 1)? "}\n":",");
+        }
+        removeDuplicates(test);
+        std::cout << "After  = {";
+        for(int i = 0; i < test.size(); i++){
+            std::cout << test[i]  << (i == (test.size() - 1)? "}\n\n":",");
+        }
     }
 
     return 0;
