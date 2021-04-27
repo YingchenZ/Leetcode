72f52c52957092da9ff2c9e13ace17ab2d0d902a
Yingchen (Eric) Zhou <ericzyc0803@gmail.com>
2021-04-27 11:04:58 +0800

    Update rangeSumOfBST.java

--- Java/easy/rangeSumOfBST.java
+++ Java/easy/rangeSumOfBST.java
@@ -31,21 +31,29 @@ class Solution {
     }
 
     // Tree = [10,5,15,3,7,13,18,1,null,6]
-    // Low = 7 High = 15       Sum = 45 
-
-    // Low = 6 High = 10       Sum = 68 
-
-    // Low = 1 High = 18       Sum = 146
+    // Low = 7 High = 15       Sum = 45
+    
+    // Low = 6 High = 10       Sum = 23
+    
+    // Low = 1 High = 18       Sum = 78
 
 
     public static void main(String[] args){
-        TreeNode root = new TreeNode(10,
-                            new TreeNode(5, new TreeNode(3, new TreeNode(1), null),new TreeNode(7, new TreeNode(6), null) ),
-                            new TreeNode(15,new TreeNode(13), new TreeNode(18)));
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(7);
+        root.left.left.left = new TreeNode(1);
+        root.left.right.left = new TreeNode(6);
+
+        root.right = new TreeNode(15);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(18);
 
         System.out.println("Tree = [10,5,15,3,7,13,18,1,null,6]");
         int[][] testSet = { {7,15}, {6,10}, {1,18}};
         for(int[] test : testSet){
+            sum = 0;
             System.out.println("Low = " + test[0] + "\tHigh = " + test[1] + "\tSum = " + rangeSumBST(root, test[0], test[1]) + '\n');
         }
     }
