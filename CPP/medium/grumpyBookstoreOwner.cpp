ec6c03592173fcef5ca7f2860ea56740134fa21b
Yingchen (Eric) Zhou <ericzyc0803@gmail.com>
2021-05-01 13:52:50 +0800

    1052 CPP Done

---
+++ CPP/medium/grumpyBookstoreOwner.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <iostream>
+
+int maxSatisfied(std::vector<int>& customers, std::vector<int>& grumpy, int X) {
+    if(customers.size() == 1) return customers[0];
+    int ans = 0;
+    int maxBoost = 0;
+    for(int i = 0; i < customers.size(); i++){
+        if(grumpy[i] == 0){
+            ans += customers[i];
+        }
+        else{
+            if(i < X){
+                maxBoost += customers[i];
+            }
+        }
+    }
+
+    int currentBoost = maxBoost;
+    for(int i = 1, j = X; i < customers.size() - (X - 1) && j < customers.size(); i++, j++){
+        if(grumpy[i - 1] == 1){
+            currentBoost -= customers[i - 1];
+        }
+        if(grumpy[j] == 1){
+            currentBoost += customers[j];
+        }
+
+        if(maxBoost < currentBoost){
+            maxBoost = currentBoost;
+        }
+    }
+    
+    return maxBoost + ans;
+}
+
+void printVector(std::vector<int> vec){
+    std::cout << "{";
+    for(int i = 0; i < vec.size(); i++){
+        std::cout << vec[i] << (i == (vec.size() - 1)?"}":",");
+    }
+    std::cout << std::endl;
+}
+
+// customers = {1,0,1,2,1,1,7,5}
+// grumpy = {0,1,0,1,0,1,0,1}
+// X = 3   Maximum Number of Customers = 16
+
+int main(){
+    std::vector<int> customers = {1,0,1,2,1,1,7,5};
+    std::vector<int> grumpy = {0,1,0,1,0,1,0,1};
+    int X = 3;
+
+    std::cout << "customers = ";
+    printVector(customers);
+
+    std::cout << "grumpy = ";
+    printVector(grumpy);
+    std::cout << "X = "<< X << "\tMaximum Number of Customers = " <<maxSatisfied(customers, grumpy, X)<< std::endl; 
+    return 0;
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 | Level | Number |
 |-------|:------:|
 |Easy|23|
-|Medium|47|
+|Medium|48|
 |Hard|4|
 
 
@@ -84,6 +84,7 @@
 |633|Sum of Square Numbers|<a href = "CPP/medium/sumOfSquareNumber.cpp">CPP</a>, <a href = "Java/medium/sumOfSquareNumber.java">Java</a>|
 |781|<a href = "CPP/medium/rabbitsInForest.cpp">Rabbits in Forest</a>|CPP|
 |1006|<a href = "CPP/medium/clumsyFactorial.cpp">Clumsy Factorial</a>|CPP|
+|1052|Grumpy Bookstore Owner|<a href = "CPP/medium/grumpyBookstoreOwner.cpp">CPP</a>|
 
 
 ## Hard
